b7625c0e1b5baed49e035bc67ffe4dbf9b69e47f
Pankaj Pachahara <143988353+pankajpachahara@users.noreply.github.com>
2025-07-08 14:56:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -137,8 +137,13 @@ resource "aws_lb" "alb" {
 resource "aws_lb_target_group" "alb_tg" {
   name        = "alb-tg-pankaj"
   target_type = "lambda"
-  lambda_arn  = aws_lambda_function.myapp.arn
 }
+
+resource "aws_lb_target_group_attachment" "lambda_attachment" {
+  target_group_arn = aws_lb_target_group.alb_tg.arn
+  target_id        = aws_lambda_function.myapp.arn
+}
+
 
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.alb.arn
