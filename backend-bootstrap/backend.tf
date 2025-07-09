309491243d1ee78cabf2cddd31c71581b782d372
pankajpachahara <143988353+pankajpachahara@users.noreply.github.com>
2025-07-09 16:31:28 +0530

    feat: Synchronize backend-bootstrap state with existing AWS resources

--- backend-bootstrap/backend.tf
+++ backend-bootstrap/backend.tf
@@ -34,3 +34,13 @@ resource "aws_dynamodb_table" "terraform_lock_table" {
     type = "S"
   }
 }
+
+output "terraform_state_bucket_name" {
+  value       = aws_s3_bucket.terraform_state_bucket.bucket
+  description = "Name of the S3 bucket for Terraform state"
+}
+
+output "terraform_lock_table_name" {
+  value       = aws_dynamodb_table.terraform_lock_table.name
+  description = "Name of the DynamoDB table for Terraform state locking"
+}
