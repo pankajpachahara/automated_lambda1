82b742026815a981d48063d3bab6829dbc9fff46
SHUBHAM KUMAR <143086699+SHUBHAMKUMAR900@users.noreply.github.com>
2025-07-21 11:02:25 +0530

    Update backend.tf

--- backend-bootstrap/backend.tf
+++ backend-bootstrap/backend.tf
@@ -1,5 +1,5 @@
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "your-unique-terraform-state-bucket"
+resource "aws_s3_bucket" "terraform_state_bucket" {
+  bucket = "pankaj-devops-lambda-tfstate-ea8d4e33"
   force_destroy = true
 
   versioning {
@@ -13,20 +13,34 @@ resource "aws_s3_bucket" "terraform_state" {
       }
     }
   }
-
-  tags = {
-    Name = "Terraform State Bucket"
-  }
 }
 
-resource "aws_s3_bucket_public_access_block" "block" {
-  bucket                  = aws_s3_bucket.terraform_state.id
+resource "aws_s3_bucket_public_access_block" "terraform_state_bucket_block" {
+  bucket                  = aws_s3_bucket.terraform_state_bucket.id
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
 
+
+resource "aws_dynamodb_table" "terraform_lock_table" {
+  name           = "pankaj-devops-lambda-tf-lock-ea8d4e33"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key      = "LockID"
+
+ attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
 output "terraform_state_bucket_name" {
-  value = aws_s3_bucket.terraform_state.id
+  value       = aws_s3_bucket.terraform_state_bucket.bucket
+  description = "Name of the S3 bucket for Terraform state"
+}
+
+output "terraform_lock_table_name" {
+  value       = aws_dynamodb_table.terraform_lock_table.name
+  description = "Name of the DynamoDB table for Terraform state locking"
 }
